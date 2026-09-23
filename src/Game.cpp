9370dd3fb327a10c4fa247b042ecec67a14a32f1
SofiaU24 <sureta24@alumnos.utalca.cl>
2026-09-23 19:53:15 -0300

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -42,11 +42,11 @@ gv(std::make_unique<GameView>(std::vector<std::string>{"images/maze-a.png","imag
 	gameState.addGhosts(ghosts);
 
 	//ghostsControl.push_back(std::make_shared<BTGhostController>(ghosts[0]));
-	ghostsControl.push_back(std::make_shared<Controller>(ghosts[0])); // Blinky lab 4
-	ghostsControl.push_back(std::make_shared<Controller>(ghosts[1]));
+	ghostsControl.push_back(std::make_shared<BlinkyController>(ghosts[0])); // Blinky lab 4
+	ghostsControl.push_back(std::make_shared<InkyController>(ghosts[1]));
 	//ghostsControl.push_back(std::make_shared<RandomController>(ghosts[2]));
 	ghostsControl.push_back(std::make_shared<PinkyController>(ghosts[2])); // lab 5
-	ghostsControl.push_back(std::make_shared<Controller>(ghosts[3]));
+	ghostsControl.push_back(std::make_shared<SueController>(ghosts[3]));
 	//  ghostsControl.push_back(std::make_shared<BlinkyController>(ghosts[0])); 	// implementar
 	//  ghostsControl.push_back(std::make_shared<InkyController>(ghosts[1])); 		// implementar
 	//  ghostsControl.push_back(std::make_shared<PinkyController>(ghosts[2]));		// implementar
